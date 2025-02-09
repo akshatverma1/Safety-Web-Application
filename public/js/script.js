ab83698e7dfb887cb33bfbb472e413ae7154cbff
akshatverma1 <akshaverma14@gmail.com>
2025-02-09 17:17:47 +0530

    106

--- public/js/script.js
+++ public/js/script.js
@@ -4,16 +4,16 @@ const greenIcon = L.icon({
     iconUrl: 'https://img.icons8.com/ios-filled/50/ff0000/circled-dot.png',
     shadowUrl: 'https://i.pinimg.com/736x/d9/23/98/d9239829a431f6b122cab13ce3cf52e2.jpg',
 
-    iconSize:     [10, 20], 
-    shadowSize:   [0, 0], 
+    iconSize: [10, 20],
+    shadowSize: [0, 0],
 });
 
 if (navigator.geolocation) {
     navigator.geolocation.watchPosition((position) => {
         const { latitude, longitude } = position.coords;
         console.log(latitude + " " + longitude);
-        const map = L.map('DisplayMap').setView([28.6315, 77.2167], 13);
-        L.marker([latitude,longitude]).addTo(map);
+        const map = L.map('DisplayMap').setView([latitude, longitude], 19);
+        L.marker([latitude, longitude]).addTo(map);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.akshat.life">AKSHAT</a> contributors'
         }).addTo(map);
@@ -25,20 +25,81 @@ if (navigator.geolocation) {
                 const dataf = await fetch("./js/delhiLocations.json");
                 const datas = await dataf.json()
                 console.log(datas.Delhi_Areas);
-                const deldata= datas.Delhi_Areas;
-                deldata.forEach(element=>{
+                const deldata = datas.Delhi_Areas;
+                deldata.forEach(element => {
                     console.log(element.latitude);
                     // L.marker([element.latitude, element.longitude],{icon: greenIcon}).addTo(map).bindPopup(element.message);
-                    L.circle([element.latitude, element.longitude],{
-                        radius: 800,
-                        color:"red"
-                    } ).addTo(map).bindPopup(element.message);
+                    if (element.safety_grade == "A") {
+                        L.circle([element.latitude, element.longitude], {
+                            radius: 1000,
+                            color: "green"
+                        }).addTo(map).bindPopup(element.name);
+                    }else if(element.safety_grade =="B"){
+                        L.circle([element.latitude, element.longitude], {
+                            radius: 1000,
+                            color: "yellow"
+                        }).addTo(map).bindPopup(element.name);
+                    }else if(element.safety_grade == "c"){
+                        L.circle([element.latitude, element.longitude], {
+                            radius: 1000,
+                            color: "blue"
+                        }).addTo(map).bindPopup(element.name);
+                    }else{
+                        L.circle([element.latitude, element.longitude], {
+                            radius: 1000,
+                            color: "red"
+                        }).addTo(map).bindPopup(element.name);
+                    }
+                })
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        async function datafechIndore() {
+            try {
+                const dataf = await fetch("./js/indoreLocation.json");
+                const datas = await dataf.json()
+                console.log(datas.Indore_Areas);
+                const deldata = datas.Indore_Areas;
+                deldata.forEach(element => {
+                    console.log(element.latitude);
+                    // L.marker([element.latitude, element.longitude],{icon: greenIcon}).addTo(map).bindPopup(element.message);
+                    if (element.safety_grade == "A") {
+                        L.circle([element.latitude, element.longitude], {
+                            radius: 200,
+                            color: "green"
+                            ,weight:0,
+                            fillOpacity:0.5
+                        }).addTo(map).bindPopup(element.name);
+                    }else if(element.safety_grade =="B"){
+                        L.circle([element.latitude, element.longitude], {
+                            radius: 200,
+                            color: "yellow"
+                            ,weight:0,
+                            fillOpacity:0.5
+                        }).addTo(map).bindPopup(element.name);
+                    }else if(element.safety_grade == "c"){
+                        L.circle([element.latitude, element.longitude], {
+                            radius: 200,
+                            color: "blue"
+                            ,weight:0,
+                            fillOpacity:0.5
+                        }).addTo(map).bindPopup(element.name);
+                    }else{
+                        L.circle([element.latitude, element.longitude], {
+                            radius: 200,
+                            color: "red"
+                            ,weight:0,
+                            fillOpacity:0.5
+                        }).addTo(map).bindPopup(element.name);
+                    }
                 })
             } catch (err) {
                 console.log(err);
             }
         }
         datafech();
+        datafechIndore();
     })
 }
 
